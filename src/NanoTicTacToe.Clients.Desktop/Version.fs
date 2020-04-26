72215954f0f4ca0c065838c3b1d805c4815f222b
iivchenko <iivchenko@live.com>
2020-04-26 22:46:49 +0200

    #2 Introduce version in repo

---
+++ src/NanoTicTacToe.Clients.Desktop/Version.fs
@@ -0,0 +1,12 @@
+﻿module NanoTicTacToe.Clients.Desktop.Version
+
+open Microsoft.Extensions.FileProviders
+open System.Reflection
+open System.IO
+
+let value = 
+    let assembly = Assembly.GetExecutingAssembly()
+    let embeddedProvider = new EmbeddedFileProvider(assembly, "NanoTicTacToe.Clients.Desktop")
+    use stream = embeddedProvider.GetFileInfo(".version").CreateReadStream()
+    use reader = new StreamReader(stream)
+    reader.ReadLine()
