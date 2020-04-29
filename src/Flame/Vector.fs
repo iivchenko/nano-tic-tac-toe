9dc7abc857bcfc9eac1f7fed78e983a4f04a85cd
iivchenko <iivchenko@live.com>
2020-04-29 19:11:02 +0200

    #8 (+)

--- src/Flame/Vector.fs
+++ src/Flame/Vector.fs
@@ -2,6 +2,7 @@
 
 type Vector<[<Measure>] 'u> =
     | Vector of x: float32<'u> * y: float32<'u>
+    static member (+) ((Vector(x1, y1)), (Vector(x2, y2))) = Vector(x1 + x2, y1 + y2)
 
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
 module Vector = 
